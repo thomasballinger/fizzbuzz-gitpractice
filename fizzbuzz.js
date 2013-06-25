9fcfa6e89bb03533c96f98363142553803851b1e
Thomas Ballinger <thomasballinger@gmail.com>
2013-06-25 11:34:31 -0400

    put loop in a function

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,9 @@
-for (var i = 0; i < 100; i++){
-    if (i < 10){
-        console.log(i);
+function loop(){
+    for (var i = 0; i < 100; i++){
+        if (i < 10){
+            console.log(i);
+        }
     }
 }
+loop()
 // this isn't really fizzbuzz
-// another comment
