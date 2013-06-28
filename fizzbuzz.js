3df71c082fb6e6e26b68eaee6a250bda1069e7af
Thomas Ballinger <thomasballinger@gmail.com>
2013-06-28 15:25:26 -0400

    add web workers

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,15 @@
-function loop(){
+function fizzbuzz(start, end){
     l = [];
-    for (var i = 0; i < 100; i++){
-        if (i % 3 == 0){
-            l.push("fizz")
+    for (var i = start; i <= end; i++){
+        if(i % 15 == 0){
+            l.push("fizzbuzz");
+        }else if (i % 3 == 0){
+            l.push("fizz");
+        }else if(i % 5 == 0){
+            l.push("buzz");
+        }else{
+            l.push(""+i);
         }
     }
-    return l
+    return l;
 }
-document.write(loop());
-// this isn't really fizzbuzz
